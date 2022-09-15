91aa642580b79ecadc3e2edaca98eb0bebdfd087
Marcelo6b <marcelouni6b@gmail.com>
2022-09-15 13:11:49 -0300

    Atualizando

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+let numero = prompt("Insira um número par:")
+
+
+if(numero % 2 === 0){
+    console.log("É um número par.")
+}
+else{
+    console.log("Esse número não é par")
+}
---
+++ exercicio3/script.js
@@ -0,0 +1,7 @@
+let num1 = prompt("Digite um número:")
+let num2 = prompt("Digite outro número")
+
+console.log(num1 > num2, "O primeiro numero é maior que segundo?")
+console.log(num1 === num2, "O primeiro numero é igual ao segundo?" )
+console.log(num1 % num2 % 2 === 0, "O primeiro numero é divisível pelo segundo?")
+console.log(num2 % num1 % 2 === 0, "O segundo numero é divisível pelo primeiro?")
